7163dbe59ba26b3e48f9f1df1305fac055ee426c
Juan Goyeneche <j.goyeneches@uniandes.edu.co>
2025-10-15 19:34:47 -0500

    cambios1

--- apps/orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import order_status, health, order_lookup
-
-urlpatterns = [
-    path("", order_lookup, name="order-lookup"),  # página HTML
-    path("api/v1/orders/<str:order_number>/status", order_status, name="order-status"),
-    path("health", health, name="health"),
-]
