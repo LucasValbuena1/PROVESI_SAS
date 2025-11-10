d8fe4ea17bd8d6199b162c2121023cf5e049524c
smontoya112 <141515975+smontoya112@users.noreply.github.com>
2025-11-10 08:35:04 -0500

    a

---
+++ apps/home/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class HomeConfig(AppConfig):
+    default_auto_field = 'django.db.apps.models.BigAutoField'
+    name = 'apps.home'
--- provesi_wms/urls.py
+++ provesi_wms/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("apps.orders.urls")),
+    path("", include("apps.home.urls")),
+    path("orders/", include("apps.orders.urls")),
 ]
